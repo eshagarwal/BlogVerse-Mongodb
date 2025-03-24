8e179ed96c6124e3efca56af6e6b95aa97f052b2
Esha Agarwal <esha.agarwal0212@gmail.com>
2025-03-24 19:06:51 +0100

    refactor aggregators to use 'author' instead of 'user' in comments grouping

--- aggregators.mongodb.js
+++ aggregators.mongodb.js
@@ -1,13 +1,5 @@
 use("blogverse");
 
-// find blogs within a date range 
-db.blogs.find({
-    createdAt: {
-      $gte: new Date("2025-03-12T00:00:00"),
-      $lte: new Date("2025-03-14T23:59:59")
-    }
-});
-
 // find blog with most comments
 db.blogs.aggregate([
     { 
@@ -20,16 +12,10 @@ db.blogs.aggregate([
     { $limit: 5 }
 ]);
 
-// find recent 5 blogs
-db.blogs.aggregate([
-    { $sort: { createdAt: -1 } },
-    { $limit: 5 }
-]);
-
 // find the most active commenters 
 db.blogs.aggregate([
     { $unwind: "$comments" },
-    { $group: { _id: "$comments.user", totalComments: { $sum: 1 } } },
+    { $group: { _id: "$comments.author", totalComments: { $sum: 1 } } },
     { $sort: { totalComments: -1 } },
     { $limit: 5 }
 ]);
@@ -46,5 +32,3 @@ db.blogs.aggregate([
     { $group: { _id: "$category", totalBlogs: { $sum: 1 } } },
     { $sort: { totalBlogs: -1 } } 
 ]);
-
-
