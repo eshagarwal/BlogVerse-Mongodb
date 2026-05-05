39424f42eb77dc78d20fafa8fa88397f84b36de0
Esha Agarwal <esha.agarwal0212@gmail.com>
2026-05-05 22:23:25 +0200

    adds formatting

--- aggregators.mongodb.js
+++ aggregators.mongodb.js
@@ -1,33 +1,33 @@
 use("blogverse");
 
 // find blog with most comments
-db.blogs.aggregate([
-    { 
-        $project: { 
-            title: 1, 
-            commentsCount: { $size: { $ifNull: ["$comments", []] } } 
-        } 
-    },
-    { $sort: { commentsCount: -1 } },
-    { $limit: 5 }
-]);
+// db.blogs.aggregate([
+//     { 
+//         $project: { 
+//             title: 1, 
+//             commentsCount: { $size: { $ifNull: ["$comments", []] } } 
+//         } 
+//     },
+//     { $sort: { commentsCount: -1 } },
+//     { $limit: 5 }
+// ]);
 
 // find the most active commenters 
-db.blogs.aggregate([
-    { $unwind: "$comments" },
-    { $group: { _id: "$comments.author", totalComments: { $sum: 1 } } },
-    { $sort: { totalComments: -1 } },
-    { $limit: 5 }
-]);
+// db.blogs.aggregate([
+//     { $unwind: "$comments" },
+//     { $group: { _id: "$comments.author", totalComments: { $sum: 1 } } },
+//     { $sort: { totalComments: -1 } },
+//     { $limit: 5 }
+// ]);
 
-// find blogs with the longest content
-db.blogs.aggregate([
-    { $project: { title: 1, contentLength: { $strLenCP: "$content" } } },
-    { $sort: { contentLength: -1 } },
-    { $limit: 5 }
-]);
+// // find blogs with the longest content
+// db.blogs.aggregate([
+//     { $project: { title: 1, contentLength: { $strLenCP: "$content" } } },
+//     { $sort: { contentLength: -1 } },
+//     { $limit: 5 }
+// ]);
 
-// count the number of blogs per category
+// // count the number of blogs per category
 db.blogs.aggregate([
     { $group: { _id: "$category", totalBlogs: { $sum: 1 } } },
     { $sort: { totalBlogs: -1 } } 
